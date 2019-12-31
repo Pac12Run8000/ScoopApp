38f0fc619c199e919152a0ada22a296439bfc359
Pac12Run8000 <gangstarr78992@>
2019-12-31 13:57:41 -0800

    Created the rounded view class and applied it to the view

---
+++ ScoopApp/View/RoundedShadowView.swift
@@ -0,0 +1,25 @@
+//
+//  RoundedShadowView.swift
+//  ScoopApp
+//
+//  Created by Michelle Grover on 12/31/19.
+//  Copyright © 2019 Norbert Grover. All rights reserved.
+//
+
+import UIKit
+
+class RoundedShadowView: UIView {
+    
+    override func awakeFromNib() {
+        setupView()
+    }
+
+    func setupView() {
+        self.layer.cornerRadius = 5.0
+        self.layer.shadowOpacity = 0.3
+        self.layer.shadowColor = UIColor.darkGray.cgColor
+        self.layer.shadowRadius = 5.0
+        self.layer.shadowOffset = CGSize(width: 0, height: 5)
+    }
+
+}
