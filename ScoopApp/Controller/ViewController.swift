33f2eb104c227abee0335631cc5c9ff59bea7ad1
Pac12Run8000 <gangstarr78992@>
2020-02-04 02:09:38 -0800

    feat: Animated the tableview and beginning search functionality

--- ScoopApp/Controller/ViewController.swift
+++ ScoopApp/Controller/ViewController.swift
@@ -18,10 +18,13 @@ class ViewController: UIViewController {
     @IBOutlet weak var actionButtonOutlet: RoundedShadowButton!
     @IBOutlet weak var mapView: MKMapView!
     @IBOutlet weak var centerMapButtonOutlet: UIButton!
+    @IBOutlet weak var destTextFieldOutlet: UITextField!
+    @IBOutlet weak var destinationCircleOutlet: CircleView!
     
     var delegate:CenterVCDelegate?
     let locationManager = CLLocationManager()
     let regionInMeters:Double = 10000
+    var tableView = UITableView()
     
     let revealingSplashView = RevealingSplashView(iconImage: UIImage(named: "launchScreenIcon")!, iconInitialSize: CGSize(width: 80, height: 80), backgroundColor: .white)
 
@@ -31,6 +34,7 @@ class ViewController: UIViewController {
         checkLocationServices()
         setupAndStartSplashAnimation()
         mapView.delegate = self
+        destTextFieldOutlet.delegate = self
         
         DataService.instance.REF_DRIVERS.observe(.value, with: { (snapshot) in
             self.loadDriverAnnotationsFromFB()
@@ -248,6 +252,107 @@ extension ViewController {
         revealingSplashView.startAnimation()
         revealingSplashView.heartAttack = true
     }
+    
+}
+
+
+// MARK:- UITextfield delegate functionality
+extension ViewController:UITextFieldDelegate {
+    
+    
+    func textFieldDidBeginEditing(_ textField: UITextField) {
+        
+        if textField == destTextFieldOutlet {
+            tableView.frame = CGRect(x: 20, y: view.frame.height, width: view.frame.width - 40, height: view.frame.height - 170)
+            tableView.layer.cornerRadius = 5.0
+            tableView.register(UITableViewCell.self, forCellReuseIdentifier: "locationCell")
+            tableView.delegate = self
+            tableView.dataSource = self
+            tableView.tag = 18
+            tableView.rowHeight = 60
+            view.addSubview(tableView)
+            animateTableView(shouldShow: true)
+            UIView.animate(withDuration: 0.2) {
+                self.destinationCircleOutlet.backgroundColor = UIColor.red
+                self.destinationCircleOutlet.borderColor = UIColor.init(red: 199/255, green: 0/255, blue: 0/255, alpha: 1.0)
+            }
+        }
+        
+        
+        
+        
+    }
+    
+    
+    func animateTableView(shouldShow:Bool) {
+        if shouldShow {
+            UIView.animate(withDuration: 0.2) {
+                self.tableView.frame = CGRect(x: 20, y: 200, width: self.view.frame.width - 40, height: self.view.frame.height - 170)
+            }
+        } else {
+            UIView.animate(withDuration: 0.2, animations: {
+                self.tableView.frame = CGRect(x: 20, y: self.view.frame.height, width: self.view.frame.width - 40, height: self.view.frame.height - 170)
+            }) { (finished) in
+                for subview in self.view.subviews {
+                    if subview.tag == 18 {
+                        subview.removeFromSuperview()
+                    }
+                }
+            }
+        }
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        if textField == destTextFieldOutlet {
+            
+            view.endEditing(true)
+        }
+        
+        return true
+    }
+    
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        if textField == destTextFieldOutlet {
+            if textField.text == "" {
+                UIView.animate(withDuration: 0.2) {
+                    self.destinationCircleOutlet.backgroundColor = UIColor.init(red: 121/255, green: 140/255, blue: 140/255, alpha: 1.0)
+                    self.destinationCircleOutlet.borderColor = UIColor.init(red: 2/255, green: 83/255, blue: 115/255, alpha: 1.0)
+                }
+            }
+        }
+    }
+    
+    func textFieldShouldClear(_ textField: UITextField) -> Bool {
+        
+        return true
+    }
+    
+}
+// MARK:- TableViewDelegate functionality
+extension ViewController:UITableViewDelegate, UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return UITableViewCell(style: .default, reuseIdentifier: "locationCell")
+    }
+    
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        animateTableView(shouldShow: false)
+       
+        print("selected")
+    }
+
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    
     
 }
 
